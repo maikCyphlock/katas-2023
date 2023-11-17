a6311aa3441585afa522032365dc8fc5f289f2b4
maikCyphlock <maikolaguilar656@gmail.com>
2023-11-16 20:52:33 -0400

    daily katas

---
+++ Tic-Tac-Toe Checker.js
@@ -0,0 +1,63 @@
+/*
+If we were to set up a Tic-Tac-Toe game, we would want to know whether the board's current state is solved, wouldn't we? Our goal is to create a function that will check that for us!
+
+Assume that the board comes in the form of a 3x3 array, where the value is 0 if a spot is empty, 1 if it is an "X", or 2 if it is an "O", like so:
+
+[[0, 0, 1],
+ [0, 1, 2],
+ [2, 1, 0]]
+
+ We want our function to return:
+    -1 if the board is not yet finished AND no one has won yet (there are empty spots),
+1 if "X" won,
+2 if "O" won,
+0 if it's a cat's game (i.e. a draw).
+You may assume that the board passed in is valid in the context of a game of Tic-Tac-Toe.
+
+*/
+
+function solution(board) {
+    // Check rows
+    for (let i = 0; i < 3; i++) {
+        if (board[i][0] === board[i][1] && board[i][1] === board[i][2] && board[i][0] !== 0) {
+            return board[i][0];
+        }
+    }
+
+    // Check columns
+    for (let i = 0; i < 3; i++) {
+        if (board[0][i] === board[1][i] && board[1][i] === board[2][i] && board[0][i] !== 0) {
+            return board[0][i];
+        }
+    }
+
+    // Check diagonals
+    if (board[0][0] === board[1][1] && board[1][1] === board[2][2] && board[0][0] !== 0) {
+        return board[0][0];
+    }
+    if (board[0][2] === board[1][1] && board[1][1] === board[2][0] && board[0][2] !== 0) {
+        return board[0][2];
+    }
+
+    // Check if board is full
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (board[i][j] === 0) {
+                return -1;
+            }
+        }
+    }
+
+    // If no winner and board is full, it's a draw
+    return 0;
+
+
+
+}
+
+console.log(solution(
+    [[0, 1, 1],
+    [0, 1, 2],
+    [2, 1, 0]]
+)
+)
